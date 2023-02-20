f35aa9a284a1b83a9eb914ef2034429faba6489c
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-02-20 16:33:32 +0200

    Added push function for stack

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,11 @@
     string[] tokens = new string[20];
     int tokenIndex = -1;
     string postfix = "";
+
+    void Push(string token)
+    {
+        tokens[++tokenIndex] = token;
+    }
 }
 
 void Main()
